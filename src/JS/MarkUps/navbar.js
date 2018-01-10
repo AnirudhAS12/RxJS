5e866feec50511e56a05b56f81588bd138158012
Anirudh Srinath <m1042946@mindtree.com>
2018-01-10 08:46:37 +0530

    updated

--- src/JS/MarkUps/navbar.js
+++ src/JS/MarkUps/navbar.js
@@ -1,28 +1,33 @@
 var navbar = () =>
 {
 var topbar=document.getElementById('topbar');
- topbar.innerHTML=`<nav class="navbar navbar-inverse">
+ topbar.innerHTML=`
+ <nav class="navbar navbar-inverse">
   <div class="container-fluid">
     <div class="navbar-header">
       <a class="navbar-brand" href="#">Cricket</a>
       </div>
     <ul class="nav navbar-nav">
       <li id ="getOnlineAPI" class="active"><a href="#" >Matches</a></li>
-      <li><a href="#">Page 1</a></li>
-      <li><a href="#">Page 2</a></li>
+      <li><a href="#">Players</a></li>
+      <li><a href="#">News</a></li>
     </ul>
-    <form class="navbar-form navbar-left">
+    <br />
+    <div class="container">
+    <form class="navbar-form navbar-center">
       <div class="input-group">
-        <input type="text" class="form-control" placeholder="Search" id="myInput">
+        <input type="text" class="form-control" placeholder="Search" id="myInput" size="200px">
         <div class="input-group-btn">
                   <button id="search" class="btn btn-default type="submit">
             <i class="glyphicon glyphicon-search"></i>
           </button>
         </div>
+        </div>
       </div>
     </form>
   </div>
 </nav>
+
 `;
 }
 export default navbar;
